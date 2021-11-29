0a89dd3f2582ac56d97416de8f9c56aafab7d5df
HeilRulez <rulez.heil@gmail.com>
2021-11-29 22:28:38 +0600

    2

--- JS_Help.js
+++ JS_Help.js
@@ -55,5 +55,88 @@ const sum = euros.reduce((total, amount) => total + amount);
 let element = document.querySelector(".class || #id || html || a[href='#']"); // не примет :visited, :link, ::before, ::after
 let element = document.querySelectorAll("те же варианты"); // можно обращаться как с масовом element[i], element.length
 
+// Возврат значения 
+
+element.getAttribute(""); // возврат значения атрибута в скобках
+element.id || element.class     // короткая запись только для этих двух атрибутов
+
+// Запись значения
+
+element.setAttribute("src", "https:\\"); // запись атрибута
+element.className = "" // короткий вариант. Производит ПЕРЕЗАПИСЬ
+
+element.textContent = "" // метод установки текста контента. 
+// Установка CSS прямо в тег, типа element.style.backgroundColor = "".
+// Что бы указать CSS свойство содержащее тире, нужно его удалить типа: border-radius,  будет: borderRadius
+
+// Работа с классами
+
+element.classList. // одно из...
+                  add(""); // добавить класс
+                  remove(""); // удалить
+                  toggle(""); // переключить (методом удаления и добавления) с вкл на выкл и обратно
+                  contains(""); // булево, содержит ли указанный класс
 
 
+// Прогулка по "DOM-у"
+
+firstChild
+lastChild
+parentNode
+children
+previousSibling
+nextSinling
+
+// Вариант прогулки по потомкам
+
+let bodyElement = document.body;
+
+for (let i = 0; i < bodyElement.children.length; i++) {  // число детей у элемента
+    let childElement = bodyElement.children[i];  // обращатся можно как к элементу массива
+    document.writeln(childElement.tagName); // writeln выводит имена тэгов со знаком перевода каретки, в документ (этот html)
+}
+
+// Вариант прогулки рекурсией
+
+function theDOMElementWalker(node) {
+    if (node.nodeType == Node.ELEMENT_NODE) {
+        console.log(node.tagName);
+        node = node.firstChild;
+
+        while (node) {
+            theDOMElementWalker(node);
+            node = node.nextSibling;
+        };
+    };
+};
+let variable = document.querySelector(".class");
+theDOMElementWalker(variable);
+
+// Созданае элементов
+
+let element = document.createElement("p"); // пока не отображается и сам по себе
+element.textContent = "какой то текст"; //можно и потом
+
+let parentElement = document.querySelector("body"); // выбор родителя для элемента
+parentElement.appendChild(element); // добавление родителю потомка. Встаёт последним ребёнком
+
+let child_old = document.querySelector("");  // выбор ребёнка на место которого вставить, сдвинув этого
+parentElement.insertBefore(element, child_old); // вставка потомка в определённое место
+                                    elemChild.nextSibling // вставит потомка после элемента elemChild. Удобно, не выяснять кого мы сдвигаем
+            
+// Удаление потомка
+
+parentElement.removeChild(element);
+element.parentNode.removeChild(element); // что бы не искать родителя удаляемого потомка
+// или просто 
+element.remove();
+
+// Клонирование элементов
+
+let parentElement = document.querySelector("body"); // кому припаяем клонированого потомка
+let element = document.querySelector("h1"); // кого клонируем
+let clon = element.cloneNode(true); // true - нужноли клонировать его потомков
+parentElement.appendChild(clon); // добавили потомка
+
+
+//проверка
