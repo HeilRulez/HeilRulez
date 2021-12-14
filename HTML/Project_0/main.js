dc87f99658ce963da26bec8be7f06c9bd5569f2b
HeilRulez <rulez.heil@gmail.com>
2021-12-15 01:19:26 +0600

    1

--- HTML/Project_0/main.js
+++ HTML/Project_0/main.js
@@ -9,6 +9,31 @@ function createElem(element) {
     return addElement;
 };
 
-let userName = prompt("Как тебя величать?", "");
-alert(`Привет, ${userName}`);
 
+// let input = prompt("Integer", 2);
+// let arr = [];
+
+// nextIterate:
+// for (let j = 2; j <= input; j++) {
+//     for(let i = 2; i < j; i++){
+//         if (j % i == 0) continue nextIterate;
+//     };
+//     arr.push(j);
+// };
+// alert(arr);
+
+
+const number = +prompt('Введите число между 0 и 3', '');
+
+switch (number) {
+    case 0:
+        alert('Вы ввели 0');
+        break;
+    case 1:
+        alert('Вы ввели 1');
+        break;
+    case 2:
+    case 3:
+        alert('Вы ввели 2, а может 3');
+        break;
+};
