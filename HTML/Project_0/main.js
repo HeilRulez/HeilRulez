4ad7cff391d13f11fa602afe46759a6d2b94a746
HeilRulez <rulez.heil@gmail.com>
2021-12-21 05:44:23 +0600

    2

--- HTML/Project_0/main.js
+++ HTML/Project_0/main.js
@@ -22,4 +22,22 @@ function createElem(element) {
 // };
 // alert(arr);
 
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu",
+};
 
+multiplyNumeric(menu);
+
+function multiplyNumeric(obj) {
+    for (let key in obj) {
+        if (typeof obj[key] == "number") {
+            menu[key] *= 2;
+        };
+    };
+}
+
+for (let item in menu) {
+    alert(menu[item]);
+};
